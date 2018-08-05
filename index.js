44bafa20a2d44e4c05fd3a5c60a4a1ecbf62806a
tekiiii <secondtime@qq.com>
2018-08-05 15:02:19 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,79 +1,118 @@
+//练习“映射”
 const countryMap = {
-  '中国' : 'cn',
-  '台湾' : 'tw',
-  '香港' : 'hk',
-  '日本' : 'jp',
-  '美国' : 'us',
-  '法国' : 'fr',
-  '英国' : 'uk'
+  '中国': 'cn',
+  '台湾': 'tw',
+  '香港': 'hk',
+  '日本': 'jp',
+  '美国': 'us',
+  '法国': 'fr',
+  '英国': 'gb'
 }
 
 const countryColorMap = {
-  '中国' : '#de2910',
-  '台湾' : '#86C166',
-  '香港' : '#c8102e',
-  '日本' : '#714C99',
-  '美国' : '#0066cc',
-  '法国' : '#002395',
-  '英国' : '#FFF'
+  '中国': '#a80d2a',
+  '台湾': '#86C166',
+  '香港': '#FF7F50',
+  '日本': '#714C99',
+  '美国': '#0066cc',
+  '法国': '#0099ff',
+  '英国': '#FFBB00'
 }
 
 Page({
   data: {
     newsItem : [],
-    newsCountry : ['中国', '台湾', '香港', '日本', '美国', '法国', '英国']
+    newsCountry : ['中国', '台湾', '香港', '日本', '美国', '法国', '英国'],
+    currentCountry : '日本',
+    menuColor: '#714C99'//初始值：日本紫
   },
 
   onLoad: function() {
-    this.getNews()
+    this.getNews(this.data.currentCountry)
+    this.setColor(this.data.currentCountry)
   },
 
   onPullDownRefresh: function () {
-    this.getNews(() => { wx.stopPullDownRefresh() })
+    this.getNews(this.data.currentCountry, () => { wx.stopPullDownRefresh() })
+  },
+  
+  onTapCountry(e) {
+    //获取国家名
+    let country = e.currentTarget.id
+    this.setData({
+      currentCountry: country
+    })
+    //显示Toast
+    wx.showToast({
+      title: country,
+    })
+    //获取新闻，更改颜色
+    this.getNews(country)
+    this.setColor(country)
   },
 
-  getNews(callback) {
+  getNews(country, callback) {
     var that = this
-    var countryName = '台湾'
-
-    wx.setNavigationBarColor({
-      frontColor: '#ffffff',
-      backgroundColor: countryColorMap[countryName],
-      animation: {
-        duration: 400,
-        timingFunc: 'easeIn'
-      }
-    })
 
     wx.request({
       url: 'https://newsapi.org/v2/top-headlines',
       
       data: {
-        country: countryMap[countryName],
+        country: countryMap[country],
         apiKey: '585bb238bef74dbdb5ad901a816ae707'
       },
       
       success: function(res) {
-        let result = res.data.articles
+        let result = res.data
         console.log(result)
-        
-        let newsItem = []
-        for (let i = 0; i < 20; i++) {
-          newsItem.push({
-            newsTitle: result[i].title,
-            newsAuthor: result[i].author == null ? '' : result[i].author,
-            newsTime: result[i].publishedAt.substr(0, 10),
-            newsImage: result[i].urlToImage
-          })
-        }
-        that.setData({
-          newsItem: newsItem
-        })
+        that.setNews(result)
       },
       
       complete: function() {
         callback && callback()
       }
     })
+  },
+
+  setNews(res) {
+    let result = res.articles
+    let newsItem = []
+    for (let i = 0; i < res.totalResults; i++) {
+      newsItem.push({
+        newsTitle: result[i].title,//标题
+        newsAuthor: result[i].author == null ? '' : result[i].author,//作者
+        newsTime: result[i].publishedAt.substr(0, 10),//日期
+        newsImage: result[i].urlToImage == null || result[i].urlToImage == '' ?
+         '/image/default_image.jpg' : result[i].urlToImage,//图片
+        newsNo: i//编号
+      })
+    }
+    this.setData({
+      newsItem: newsItem
+    })
+  },
+
+  setColor(countryName) {
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: countryColorMap[countryName],
+      // animation: {
+      //   duration: 400,
+      //   timingFunc: 'easeIn'
+      // }
+    })
+    //设置菜单栏颜色
+    this.setData({
+      menuColor: countryColorMap[countryName]
+    })
+  },
+
+  onTapWebNews(e) {
+    let str = e.currentTarget.id
+    //console.log(str)
+    wx.navigateTo({
+      url: '/pages/webNews/webNews?newsNo=' + str + '&country=' + this.data.currentCountry,
+    })
   }
 })
+ 
